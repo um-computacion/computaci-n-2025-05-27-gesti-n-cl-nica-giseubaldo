d08f79fb29a0c4422539476b6a314dc8dc1332e6
Gisela Ubaldo <g.ubaldo@alumno.um.edu.ar>
2025-06-17 11:18:36 -0300

    Implementar excepciones personalizadas

---
+++ src/excepciones.py
@@ -0,0 +1,34 @@
+class PacienteNoEncontradoException(Exception):
+    pass
+
+
+class MedicoNoEncontradoException(Exception):
+    pass
+
+
+class MedicoNoDisponibleException(Exception):
+    pass
+
+
+class TurnoOcupadoException(Exception):
+    pass
+
+
+class RecetaInvalidaException(Exception):
+    pass
+
+
+class PacienteDuplicadoException(Exception):
+    pass
+
+
+class MedicoDuplicadoException(Exception):
+    pass
+
+
+class EspecialidadDuplicadaException(Exception):
+    pass
+
+
+class DatosInvalidosException(Exception):
+    pass
