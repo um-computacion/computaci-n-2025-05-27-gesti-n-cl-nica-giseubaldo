024dc91cde185d2158e4409fc1e1797067052a02
Gisela Ubaldo <g.ubaldo@alumno.um.edu.ar>
2025-06-16 23:42:11 -0300

    Implementar clase medico

---
+++ src/especialidad.py
@@ -0,0 +1,14 @@
+class Especialidad:
+    def __init__(self, tipo, dias):
+        self._tipo = tipo
+        self._dias = [dia.lower() for dia in dias]
+    
+    def obtener_especialidad(self):
+        return self._tipo
+    
+    def verificar_dia(self, dia):
+        return dia.lower() in self._dias
+    
+    def __str__(self):
+        dias_str = ", ".join(self._dias)
+        return f"{self._tipo} (Días: {dias_str})"
